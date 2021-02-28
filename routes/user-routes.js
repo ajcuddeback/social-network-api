37087ef6c806d90e340ce8d2fa7bfa671c5da937
ajcudd <ajcuddeback@gmail.com>
2021-02-28 10:37:17 -0500

    added user routes

---
+++ routes/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend
+} = require('../controllers/user-controllers');
+
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route('/:userid/friends/friendId')
+    .post(addFriend);
+
+
+module.exports = router;
